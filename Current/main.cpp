cfb79a7dd4e2adae2b54faad1a6ac20e0296783f
sc0rp10n-lappy-wslubuntu <pragyan52yadav@gmail.com>
2023-07-23 08:57:02 +0530

    some

---
+++ Current/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            ans = max(ans, a[i] + a[j]);
+        }
+    }
+    cout << ans << endl;
+}
